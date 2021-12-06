22b90c832717d649a60499cfac5f2506e26d51c9
Ricardo Oriol Rocabert <ricardooriol@RicardoOriol.local>
2021-12-06 17:41:56 +0100

    creating ddoctor

--- src/ui/ClientSendCharactersViaNetwork.java
+++ src/ui/ClientSendCharactersViaNetwork.java
@@ -9,10 +9,15 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import pojos.Patient;
+import static utils.InputOutput.getStringFromKeyboard;
 
 /**
  *
@@ -62,7 +67,9 @@ public class ClientSendCharactersViaNetwork {
                 dout.writeInt(choice);
                 switch (choice) {
                     case 1:
+                        int roleId = 2;
                         String response = ui.Main.newUser();
+                        dout.writeInt(roleId);
                         dout.writeUTF(response);
                         break;
                     case 2:
@@ -70,8 +77,8 @@ public class ClientSendCharactersViaNetwork {
                         dout.writeUTF(response_login);
                         String okay = dint.readUTF();
                         System.out.println(okay);
-                        if (okay.equals("Welcome patient !")) {
-                        
+                        if (okay.equals("Welcome doctor !")) {
+                            menuDoctor();
                            //releaseResources2(outputStream, console, inputStream, dint, dout);
                         }
                         /*socket = new Socket("192.168.68.112", 9000);
@@ -97,6 +104,110 @@ public class ClientSendCharactersViaNetwork {
             Logger.getLogger(ClientSendCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+        private static void menuDoctor() throws Exception {
+        
+        InputStream console2;
+        InputStream inputStream2;
+        OutputStream outputStream2;
+        DataInputStream dint2;
+        DataOutputStream dout2;
+        ObjectInputStream objectInputStream = null;
+        
+       
+        try {
+            console2 = (System.in);
+            inputStream2 = socket.getInputStream();
+            outputStream2 = socket.getOutputStream();
+            dint2 = new DataInputStream(inputStream2);
+            dout2 = new DataOutputStream(outputStream2);
+            objectInputStream = new ObjectInputStream(inputStream2);
+            
+            while (true) {
+            System.out.println("What would you like to do?");
+            System.out.println("1. Search patient by name");
+            System.out.println("2. Search EMG by name");
+            System.out.println("3. Search ECG by name");
+            System.out.println("4. Search Form by name");
+            System.out.println("5. Delete patient");
+            System.out.println("6. Change your userName");
+            System.out.println("7. Change your password");
+            System.out.println("8. Go back");
+                Integer choice = new Integer(0);
+                boolean wrongtext = false;
+                do {
+                    System.out.println("Introduce the number of the option you would like to choose: ");
+                    try {
+                        choice = Character.getNumericValue(console2.read());
+                        wrongtext = false;
+                    } catch (NumberFormatException ex) {
+                        wrongtext = true;
+                        System.out.println("It's not an int, please enter an int");
+                    }
+                } while (choice < 1 || choice > 8 || wrongtext);
+                dout2.writeInt(choice);
+                switch (choice) {
+                    case 1:
+                        String response = ui.Main.searchPatientByName();
+                        dout2.writeUTF(response);
+                        List<Patient> patientList = new ArrayList <Patient>();
+                        Object tmp;
+                        while ((tmp = objectInputStream.readObject()) != null) {
+                            Patient patient = (Patient) tmp;
+                            patientList.add(patient);
+                        }
+                        ui.Main.searchPatientByName2(patientList);
+                        break;
+                    case 2:
+                        String response_EMG_ECG = ui.Main.addEMG_addECG();
+                        dout2.writeUTF(response_EMG_ECG);
+                        BITalino.BitalinoDemo.main(socket);
+                        break;
+                    case 3:
+                        List<Emg> emgList = new ArrayList <Emg>();
+                        Object tmp;
+                        while ((tmp = objectInputStream.readObject()) != null) {
+                            Emg emg = (Emg) tmp;
+                            emgList.add(emg);
+                        }
+
+                        ui.Main.searchEMGByName_patient(emgList);
+                        break;
+                    case 4:
+                        List<Ecg> ecgList = new ArrayList <Ecg>();
+                        Object tmp2;
+                        while ((tmp2 = objectInputStream.readObject()) != null) {
+                            Ecg ecg = (Ecg) tmp2;
+                            ecgList.add(ecg);
+                        }
+                        ui.Main.searchECGByName_patient(ecgList);
+                        break;
+                    case 5:
+
+                    case 6:
+                        String response_newUser = ui.Main.changeUsername();
+                        dout2.writeUTF(response_newUser);
+                        okay = dint2.readUTF();
+                        System.out.println(okay);
+                        break;
+                    case 7:
+                        String response_newPassword = ui.Main.changePassword();
+                        dout2.writeUTF(response_newPassword);
+                        okay = dint2.readUTF();
+                        System.out.println(okay);
+                        break;
+
+                    case 8:
+                        //releaseResources2(outputStream2, console2, inputStream2, dint2, dout2);
+                        return;
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ClientSendCharactersViaNetwork2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
     
      private static void releaseResources(OutputStream outputStream, InputStream console, InputStream inputStream, InputStream console2, DataInputStream dint, DataOutputStream dout, Socket socket) {
         try {
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,6 +7,8 @@ package ui;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
+import pojos.Patient;
 import static utils.InputOutput.getFloatFromKeyboard;
 import static utils.InputOutput.getIntFromKeyboard;
 import static utils.InputOutput.getStringFromKeyboard;
@@ -16,15 +18,22 @@ import static utils.InputOutput.getStringFromKeyboard;
  * @author RAQUEL
  */
 public class Main {
-     public static void menu() throws Exception {
+
+    public static void menu() throws Exception {
         System.out.println("Welcome to our database!");
 
     }
 
     public static String newUser() throws Exception {
         String response = "";
-        System.out.println("Please, enter the following information: ");
-      
+        System.out.println("Please, enter your full name: ");
+        String name = getStringFromKeyboard("Full name: ");
+        System.out.println("Please type the new user information: ");
+        String username = getStringFromKeyboard("DNI (this will be your username): ");
+        String password = getStringFromKeyboard("Password: ");
+        Integer roleId = 2;
+        response = name + "," + username + "," + password + "," + roleId;
+
         return response;
     }
 
@@ -35,6 +44,33 @@ public class Main {
         String password = getStringFromKeyboard("Password: ");
         response = username + "," + password;
         return response;
+    }
+
+    public static String changeUsername() {
+        String newName = getStringFromKeyboard("Introduce your new username: ");
+        return newName;
+    }
+
+    public static String searchPatientByName() {
+        System.out.println("Please, enter the following information");
+        String name = getStringFromKeyboard("Enter the name of the patient you want to search: ");
+        return name;
+    }
+
+    public static void searchPatientByName2(List<Patient> patientList) {
+        if (!patientList.isEmpty()) {
+            for (Patient patient : patientList) {
+                System.out.println(patient);
+            }
+        } else {
+            System.out.println("There are no patients with that name.");
+        }
+
+    }
+
+    public static String changePassword() {
+        String newPassword = getStringFromKeyboard("Introduce your new password: ");
+        return newPassword;
     }
 
 }
